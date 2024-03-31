fdd9bbca162ed336c49beef7bdad9819bf0b7c37
Aibek Aldabergenov <aaldaber@gmail.com>
2024-04-01 00:04:02 +0300

    adding html representation for displaying the Post in the admin site

--- cbis/community/admin.py
+++ cbis/community/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Community, Post, TemplateField, PostField, PostTemplate, Field
+from django.utils.html import format_html
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'community', 'author', 'created_at', 'edited_at']
+
+    readonly_fields = ('html_representation',)
+
+    def html_representation(self, obj):
+        return format_html(obj.get_html_content())
+
 
 admin.site.register(Community)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(TemplateField)
 admin.site.register(PostField)
 admin.site.register(PostTemplate)
