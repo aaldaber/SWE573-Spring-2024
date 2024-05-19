48ce577902d5285377b5f5944f8c4c2fe9651e4a
Aibek Aldabergenov <aaldaber@gmail.com>
2024-05-19 17:34:36 +0300

    Update admin.py

--- cbis/community/admin.py
+++ cbis/community/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from .models import Community, Post, TemplateField, PostField, PostTemplate
-from django.utils.html import format_html
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'community', 'author', 'date_created', 'date_edited']
-
-    readonly_fields = ('html_representation',)
-
-    def html_representation(self, obj):
-        return format_html(obj.get_html_content())
 
 
 admin.site.register(Community)
