6feec1a82c218bfbcf988d13ed7697e50b216423
Aibek Aldabergenov <aaldaber@gmail.com>
2024-05-20 10:03:14 +0300

    adding testing custom template creation

--- cbis/community/tests.py
+++ cbis/community/tests.py
@@ -1,3 +1,4 @@
+import json
 from django.test import TestCase
 from .models import User, Community, PostTemplate
 from django.urls import reverse
@@ -45,4 +46,20 @@ class CommunityTest(TestCase):
         self.assertEqual(the_community.name, "community")
         self.assertEqual(the_community.post_templates.count(), 1)
         self.assertEqual(the_community.post_templates.all()[0].name, "Default template")
+
+    def test_create_custom_template(self):
+        template_payload = '{"payload": {"values":["text field","text area","integer field","float field","boolean field","date field","datetime field","file field","image field","geolocation field"],"reqs":[true,false,true,false,false,false,false,true,false,true],"theorder":["text","textarea","integer","float","boolean","date","datetime","file","image","geolocation"],"template_name":"Custom template"}}'
+        user = create_user(email=self.email, password=self.password)
+        self.client.login(email=self.email, password=self.password)
+        response = self.client.post(reverse("community_create"), {"name": "community", "description": "description",
+                                                                  "is_public": "true"})
+        the_community = Community.objects.all()[0]
+        self.assertEqual(the_community.name, "community")
+        response = self.client.post(reverse("community_templates_create", args=(the_community.id,)),
+                                    data=json.loads(template_payload), content_type='application/json',
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+                                    )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(the_community.post_templates.count(), 2)
+        self.assertEqual(the_community.post_templates.all()[1].name, "Custom template")
 
