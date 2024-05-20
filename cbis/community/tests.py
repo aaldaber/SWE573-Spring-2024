433cf10a1f2e3cd24fac3ff81528a63c38d973a9
Aibek Aldabergenov <aaldaber@gmail.com>
2024-05-20 09:37:46 +0300

    testing if default template is added with new community

--- cbis/community/tests.py
+++ cbis/community/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import User, Community
+from .models import User, Community, PostTemplate
 from django.urls import reverse
 
 
@@ -9,25 +9,24 @@ def create_user(email, password):
 
 
 class CommunityTest(TestCase):
+    password = "comm_pass_$123"
+    email = "community_user@local.local"
+
     def test_new_community(self):
-        user_password = "comm_pass_$123"
-        email = "community_user@local.local"
-        user = create_user(email=email, password=user_password)
-        self.client.login(email=email, password=user_password)
+        user = create_user(email=self.email, password=self.password)
+        self.client.login(email=self.email, password=self.password)
         response = self.client.post(reverse("community_create"), {"name": "community", "description": "description",
                                                                   "is_public": "true"})
         self.assertEqual(Community.objects.all()[0].name, "community")
-        self.assertEqual(Community.objects.all()[0].owner.email, email)
+        self.assertEqual(Community.objects.all()[0].owner.email, self.email)
         # check adding another community with the same name
         response = self.client.post(reverse("community_create"), {"name": "community", "description": "description",
                                                                   "is_public": "true"})
         self.assertEqual(Community.objects.all().count(), 1)
 
     def test_edit_community(self):
-        user_password = "comm_pass_$123"
-        email = "community_user@local.local"
-        user = create_user(email=email, password=user_password)
-        self.client.login(email=email, password=user_password)
+        user = create_user(email=self.email, password=self.password)
+        self.client.login(email=self.email, password=self.password)
         response = self.client.post(reverse("community_create"), {"name": "community", "description": "description",
                                                                   "is_public": "true"})
         the_community = Community.objects.all()[0]
@@ -36,4 +35,14 @@ class CommunityTest(TestCase):
                                                                   "is_public": "false"})
         self.assertEqual(Community.objects.all()[0].name, "community_edited")
         self.assertEqual(Community.objects.all()[0].is_public, False)
+
+    def test_new_community_default_template_created(self):
+        user = create_user(email=self.email, password=self.password)
+        self.client.login(email=self.email, password=self.password)
+        response = self.client.post(reverse("community_create"), {"name": "community", "description": "description",
+                                                                  "is_public": "true"})
+        the_community = Community.objects.all()[0]
+        self.assertEqual(the_community.name, "community")
+        self.assertEqual(the_community.post_templates.count(), 1)
+        self.assertEqual(the_community.post_templates.all()[0].name, "Default template")
 
